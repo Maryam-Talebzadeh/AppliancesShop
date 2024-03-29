c4e5aaffeeadb9a255a20c39800c62d71abeb338
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-03-28 22:55:07 -0700

    Added ProductCategoryService.

---
+++ SM.Domain.Services/ProductCategoryAgg/ProductCategoryService.cs
@@ -0,0 +1,33 @@
+﻿using SM.Domain.Core.ProductCategoryAgg.DTOs;
+using SM.Domain.Core.ProductCategoryAgg.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Services.ProductCategoryAgg
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        public void Create(CreateProductCategoryDTO command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(EditProductCategoryDTO command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategoryDetailDTO GetDetail(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryDTO> Search(SearchProductCategoryDTO searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
