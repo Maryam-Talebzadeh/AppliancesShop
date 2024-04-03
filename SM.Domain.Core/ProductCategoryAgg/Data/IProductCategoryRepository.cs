ec4efbc1ce14e14550aff190547fc4a40301b576
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-03 06:57:08 -0700

    I Changed input value type of IsExist method.

--- SM.Domain.Core/ProductCategoryAgg/Data/IProductCategoryRepository.cs
+++ SM.Domain.Core/ProductCategoryAgg/Data/IProductCategoryRepository.cs
@@ -1,15 +1,16 @@
 ﻿using ServiceFramework;
 using SM.Domain.Core.ProductCategoryAgg.DTOs;
 using SM.Domain.Core.ProductCategoryAgg.Entities;
+using System.Linq.Expressions;
 
 namespace SM.Domain.Core.ProductCategoryAgg.Data
 {
     public interface IProductCategoryRepository
     {
         void Create(CreateProductCategoryDTO productCategory);
-        ProductCategory GetBy(int id);
+        ProductCategoryDTO GetBy(int id);
         void Save();
-        bool IsExist(string name);
+        bool IsExist(Expression<Func<ProductCategoryDTO, bool>> expression);
 
     }
 }
