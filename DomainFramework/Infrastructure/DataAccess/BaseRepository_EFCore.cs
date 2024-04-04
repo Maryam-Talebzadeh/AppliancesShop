4826a0e972e286b25a6fc4375c593f123a04a3ae
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-03 22:31:54 -0700

    Implemented General BaseRepository.

---
+++ DomainFramework/Infrastructure/DataAccess/BaseRepository_EFCore.cs
@@ -0,0 +1,31 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Framework.Infrastructure.DataAccess
+{
+    public class BaseRepository_EFCore<TKey, T> : IRepository<TKey, T> where T : class
+    {
+        private readonly DbContext _context;
+
+        public BaseRepository_EFCore(DbContext context)
+        {
+            _context = _context;
+        }
+
+        public bool IsExist(Expression<Func<T, bool>> expression)
+        {
+           return _context.Set<T>().Any(expression);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
