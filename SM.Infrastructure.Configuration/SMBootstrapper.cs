26e38b380a2689de3a62d852072c9976d73fde46
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 08:09:23 -0700

    I want to wire up each module inside itself. I mean that each module has its own configuration inside itself. so I created InFrastructureConfiguration and I'll create such class in other modules.

---
+++ SM.Infrastructure.Configuration/SMBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SM.Domain.Core.ProductCategoryAgg.Data;
+using SM.Infrastructure.DataAccess.Repos.EFCore.ProductCategoryAgg;
+using SM.Infrastructure.DB.SqlServer.EFCore.Contexts;
+
+namespace SM.Infrastructure.Configuration
+{
+    public class SMBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+
+            #region DbContext
+
+            services.AddDbContext<ShopContext>(options =>
+            options.UseSqlServer(connectionString)
+            );
+
+            #endregion
+        }
+
+
+    }
+}
