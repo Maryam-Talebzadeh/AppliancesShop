efb4700157400b89546b5223f32b983c26f5426d
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 08:40:21 -0700

    Registered ProductCategoryService following the last commit.

--- SM.Infrastructure.Configuration/SMBootstrapper.cs
+++ SM.Infrastructure.Configuration/SMBootstrapper.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SM.Domain.Core.ProductCategoryAgg.Data;
+using SM.Domain.Core.ProductCategoryAgg.Services;
+using SM.Domain.Services.ProductCategoryAgg;
 using SM.Infrastructure.DataAccess.Repos.EFCore.ProductCategoryAgg;
 using SM.Infrastructure.DB.SqlServer.EFCore.Contexts;
 
@@ -11,6 +13,7 @@ namespace SM.Infrastructure.Configuration
         public static void Configure(IServiceCollection services, string connectionString)
         {
             services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+            services.AddScoped<IProductCategoryService, ProductCategoryService>();
 
             #region DbContext
 
