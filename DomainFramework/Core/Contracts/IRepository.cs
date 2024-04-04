a439967c0a8e25375654a5c17047387598fe111f
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-03 21:08:44 -0700

    Added base general IRepository.

---
+++ DomainFramework/Core/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainFramework.Core.Contracts
+{
+    public interface IRepository<TKey, T> where T : class
+    {
+        public bool IsExist(Expression<Func<T, bool>> expression);
+        public void Save();
+    }
+}
