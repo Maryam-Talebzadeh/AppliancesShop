c9737226d8f5c142c38077bbf857d5551f41d717
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-06 10:26:56 -0700

    Added razorpage for controll productCategory part.

---
+++ ServiceHost.RazorPages/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.RazorPages.Pages.Shop.ProductCategories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
