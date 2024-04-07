80389a1aa95afaceec8acb388396c1484794247d
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-07 00:21:55 -0700

    Wrote the OnGet handler of ProductCategories-Index.

--- ServiceHost.RazorPages/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
+++ ServiceHost.RazorPages/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SM.Domain.Core.ProductCategoryAgg.AppServices;
+using SM.Domain.Core.ProductCategoryAgg.DTOs;
 
 namespace ServiceHost.RazorPages.Areas.Administration.Pages.Shop.ProductCategories
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductCategoryAppService _productCategoryAppService;
+        public List<ProductCategoryDTO> ProductCategories;
+
+        public IndexModel(IProductCategoryAppService productCategoryAppService)
         {
+            _productCategoryAppService = productCategoryAppService;
+        }
+
+        public void OnGet(SearchProductCategoryDTO search)
+        {
+            ProductCategories = _productCategoryAppService.Search(search);
         }
     }
 }
