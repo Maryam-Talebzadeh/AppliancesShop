0640c376f12cc3625962a8ae33823fe6774183be
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-07 00:02:55 -0700

    Added  AppService  for ProductCategory

---
+++ SM.Domain.Core/ProductCategoryAgg/AppServices/IProductCategoryAppService.cs
@@ -0,0 +1,18 @@
+﻿using Base_Framework.Domain.Services;
+using SM.Domain.Core.ProductCategoryAgg.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.ProductCategoryAgg.AppServices
+{
+    public interface IProductCategoryAppService
+    {
+        OperationResult Create(CreateProductCategoryDTO command);
+        OperationResult Edit(EditProductCategoryDTO command);
+        List<ProductCategoryDTO> Search(SearchProductCategoryDTO searchModel);
+        ProductCategoryDetailDTO GetDetail(long id);
+    }
+}
