62d548303fee1fabc8fd5b2ea47f191613406ef2
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-08 02:08:53 -0700

    I set eventListener for submit that if  data-ajax="true", it works and will call the CallBackHandler.

--- ServiceHost.RazorPages/wwwroot/js/custom.js
+++ ServiceHost.RazorPages/wwwroot/js/custom.js
@@ -28,6 +28,43 @@ function hideModal() {
     $("#MainModal").modal("hide");
 }
 
+$(document).on("submit",
+'form[data-ajax="true"]',
+function (e) {
+    e.preventDefault();
+    var form = $(this);
+    const method = form.attr("method").toLocaleLowerCase();
+    const url = form.attr("action");
+    var action = form.attr("data-action");
+
+    if (method === "get") {
+        const data = form.serializeArray();
+        $.get(url,
+            data,
+            function (data) {
+                CallBackHandler(data, action, form);
+            });
+    } else {
+        var formData = new FormData(this);
+        $.ajax({
+            url: url,
+            type: "post",
+            data: formData,
+            enctype: "multipart/form-data",
+            dataType: "json",
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                CallBackHandler(data, action, form);
+            },
+            error: function (data) {
+                alert("خطایی رخ داده است. لطفا با مدیر سیستم تماس بگیرید.");
+            }
+        });
+    }
+    return false;
+});
+
 function CallBackHandler(data, action, form) {
     switch (action) {
         case "Message":
