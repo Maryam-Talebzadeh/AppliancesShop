acdc264d7ebc30b5c9209ed625ad6f781771a404
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-08 01:08:57 -0700

    I wrote the LoadModal that if url start with "ShowModal" hash, It fetches the address that comes after the hash and then calls it. This address is supposed to be the address of the handler that returns a partial view, and this partial view is actually the content in the middle of the modal(MainModal in AdminLayout)

--- ServiceHost.RazorPages/wwwroot/js/custom.js
+++ ServiceHost.RazorPages/wwwroot/js/custom.js
@@ -1,3 +1,25 @@
+var SinglePage = {};
+
+SinglePage.LoadModal = function () {
+    var url = window.location.hash.toLowerCase();
+    if (!url.startsWith("#showmodal")) {
+        return;
+    }
+    url = url.split("showmodal=")[1];
+    $.get(url,
+        null,
+        function (htmlPage) {
+            $("#ModalContent").html(htmlPage);
+            const container = document.getElementById("ModalContent");
+            const forms = container.getElementsByTagName("form");
+            const newForm = forms[forms.length - 1];
+            $.validator.unobtrusive.parse(newForm);
+            showModal();
+        }).fail(function (error) {
+            alert("خطایی رخ داده، لطفا با مدیر سیستم تماس بگیرید.");
+        });
+};
+
 function showModal() {
     $("#MainModal").modal("show");
 }
