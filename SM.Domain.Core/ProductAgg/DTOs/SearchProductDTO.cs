35f31b57f94974de76e4e2fa4558031cf41a4c51
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-09 05:29:45 -0700

    Added  SearchProductDTO

---
+++ SM.Domain.Core/ProductAgg/DTOs/SearchProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.ProductAgg.DTOs
+{
+    public class SearchProductDTO
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public long CategoryId { get; set; }
+    }
+}
