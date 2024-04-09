681bb3114821996302e16a576146391aa90eb766
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-09 00:56:52 -0700

    Added validations.

--- SM.Domain.Core/ProductCategoryAgg/DTOs/CreateProductCategoryViewModel.cs
+++ SM.Domain.Core/ProductCategoryAgg/DTOs/CreateProductCategoryViewModel.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Base_Framework.Domain.Core;
+using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
 namespace SM.Domain.Core.ProductCategoryAgg.DTOs
 {
     public class CreateProductCategoryViewModel
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; set; }
+
         public string Description { get; set; }
-        public IFormFile Picture { get; set; }
+        public IFormFile? Picture { get; set; }
         public string PictureAlt { get; set; }
         public string PictureTitle { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Keywords { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescription { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Slug { get; set; }
     }
+}
 }
