241d0b19c31dfddb956c74c821d7cee4796d72b5
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-09 05:28:19 -0700

    Added ProductDTO

---
+++ SM.Domain.Core/ProductAgg/DTOs/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.ProductAgg.DTOs
+{
+    public class ProductDTO
+    {
+        public long Id { get; set; }
+        public string Picture { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Category { get; set; }
+        public long CategoryId { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
