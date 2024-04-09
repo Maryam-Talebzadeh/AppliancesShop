cedacf1e045c1b609f2aa3b291ad0119aa0273b0
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-09 05:36:23 -0700

    Added IProductService

---
+++ SM.Domain.Core/ProductAgg/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Base_Framework.Domain.Services;
+using SM.Domain.Core.ProductAgg.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.ProductAgg.Services
+{
+    public interface IProductService
+    {
+        OperationResult Create(CreateProductDTO command);
+        OperationResult Edit(EditProductDTO command);
+        EditProductDTO GetDetails(long id);
+        List<ProductDTO> GetProducts();
+        List<ProductDTO> Search(SearchProductDTO searchModel);
+    }
+}
