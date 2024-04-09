aaae5217e70c4e7fa937eae4e15bc86ecb2344d8
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-09 05:41:51 -0700

    Implemented IProductService.

---
+++ SM.Domain.Services/ProductAgg/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using Base_Framework.Domain.Services;
+using SM.Domain.Core.ProductAgg.DTOs;
+using SM.Domain.Core.ProductAgg.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Services.ProductAgg
+{
+    public class ProductService : IProductService
+    {
+        public OperationResult Create(CreateProductDTO command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditProductDTO command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditProductDTO GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDTO> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDTO> Search(SearchProductDTO searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
