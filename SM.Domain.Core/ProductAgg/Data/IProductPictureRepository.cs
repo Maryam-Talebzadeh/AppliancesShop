7acda7c79a8113145427f89a3d30b6789fe0cf2b
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-11 04:44:08 -0700

    Added IProductPictureRepository

---
+++ SM.Domain.Core/ProductAgg/Data/IProductPictureRepository.cs
@@ -0,0 +1,15 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using SM.Domain.Core.ProductAgg.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.ProductAgg.Data
+{
+    public interface IProductPictureRepository : IRepository<long, ProductPicture>
+    {
+
+    }
+}
