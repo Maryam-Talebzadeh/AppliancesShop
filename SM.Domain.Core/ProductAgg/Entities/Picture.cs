eb7753c00528b9f43323b550a795658af1f71662
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-11 04:40:39 -0700

    Added picture entity.

---
+++ SM.Domain.Core/ProductAgg/Entities/Picture.cs
@@ -0,0 +1,32 @@
+﻿using Base_Framework.Domain.Core.Entities;
+
+namespace SM.Domain.Core.ProductAgg.Entities
+{
+    public class ProductPicture : BaseEntity
+    {
+        public long ProductId { get; private set; }
+        public string Picture { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public Product Product { get; private set; }
+
+        public ProductPicture(long productId, string picture, string pictureAlt, string pictureTitle)
+        {
+            ProductId = productId;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+        }
+
+        public void Edit(long productId, string picture, string pictureAlt, string pictureTitle)
+        {
+            ProductId = productId;
+
+            if (!string.IsNullOrWhiteSpace(picture))
+                Picture = picture;
+
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+        }
+    }
