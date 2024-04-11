a492311ae316f5f2b875886c6d3247c31fd84693
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-11 05:47:48 -0700

    Separated CreateProductPictureDTO and CreateProductPictureViewModel, as I said the difference before.

--- SM.Domain.Core/ProductAgg/DTOs/Product/CreateProductViewModel.cs
+++ SM.Domain.Core/ProductAgg/DTOs/Product/CreateProductViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using Base_Framework.Domain.Core;
 using Microsoft.AspNetCore.Http;
-using SM.Domain.Core.ProductAgg.DTOs.Product;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,16 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SM.Domain.Core.ProductAgg.DTOs.ProductPicture
+namespace SM.Domain.Core.ProductAgg.DTOs.Product
 {
-    public class CreateProductPictureDTO
+    public class CreateProductViewModel
     {
+        [Range(1, 100000, ErrorMessage = ValidationMessages.IsRequired)]
         public long ProductId { get; set; }
 
-        public string Picture { get; set; }
+        public IFormFile Picture { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
     }
 }
