ca3dc19c2adda583c3b566c2d18212f1b95a36ca
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-11 05:31:46 -0700

    Separated CreateProductPictureDTO and CreateProductPictureViewModel, as I said the difference before.

--- SM.Domain.Core/ProductAgg/DTOs/ProductPicture/CreateProductPictureViewModel.cs
+++ SM.Domain.Core/ProductAgg/DTOs/ProductPicture/CreateProductPictureViewModel.cs
@@ -10,14 +10,17 @@ using System.Threading.Tasks;
 
 namespace SM.Domain.Core.ProductAgg.DTOs.ProductPicture
 {
-    public class CreateProductPictureDTO
+    public class CreateProductPictureViewModel
     {
+        [Range(1, 100000, ErrorMessage = ValidationMessages.IsRequired)]
         public long ProductId { get; set; }
 
-        public string Picture { get; set; }
+        public IFormFile Picture { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
     }
 }
