d68ba6ee0895db82ea04cc0f2af8492063d0770e
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-12 01:59:37 -0700

    I started to write Slider UseCase. so at first, I Wrote the Slide and SlidePicture entity.

---
+++ SM.Domain.Core/SliderAgg/Entities/Slide.cs
@@ -0,0 +1,46 @@
+﻿using Base_Framework.Domain.Core.Entities;
+using SM.Domain.Core.ProductCategoryAgg.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.SliderAgg.Entities
+{
+    public class Slide : BaseEntity
+    {
+        public string Heading { get; private set; }
+        public string Title { get; private set; }
+        public string Text { get; private set; }
+        public string BtnText { get; private set; }
+        public string Link { get; private set; }
+        public bool IsRemoved { get; private set; }
+        public long PictureId { get; private set; }
+        public SlidePicture Picture { get; private set; }
+
+        public Slide(long pictureId,  string heading,
+            string title, string text, string link, string btnText)
+        {
+            PictureId = pictureId;
+            Heading = heading;
+            Title = title;
+            Text = text;
+            BtnText = btnText;
+            Link = link;
+            IsRemoved = false;
+        }
+
+        public void Edit(long pictureId, string heading,
+            string title, string text, string link, string btnText)
+        {
+            PictureId= pictureId;
+            Heading = heading;
+            Title = title;
+            Text = text;
+            BtnText = btnText;
+            Link = link;
+        }
+
+    }
+}
---
+++ SM.Domain.Core/SliderAgg/Entities/SlidePicture.cs
@@ -0,0 +1,31 @@
+﻿using Base_Framework.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.Domain.Core.SliderAgg.Entities
+{
+    public class SlidePicture : BaseEntity
+    {
+        public SlidePicture(string name, string title, string alt)
+        {
+            Name = name;
+            Title = title;
+            Alt = alt;
+        }
+
+        public string Name { get; private set; }
+        public string Title { get; private set; }
+        public string Alt { get; private set; }
+
+       public void Edit(string name, string title, string alt)
+        {
+            Name = name;
+            Title = title;
+            Alt = alt;
+        }
+
+    }
+}
