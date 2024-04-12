db71ff2bdd762791a3a5ab7b42c6e34ec0f07870
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-12 06:54:41 -0700

    Registered SliderAgg interfaces in Bootstrapper.

--- SM.Infrastructure.Configuration/SMBootstrapper.cs
+++ SM.Infrastructure.Configuration/SMBootstrapper.cs
@@ -2,16 +2,22 @@
 using Microsoft.Extensions.DependencyInjection;
 using SM.Domain.AppServices.ProductAgg;
 using SM.Domain.AppServices.ProductCategoryAgg;
+using SM.Domain.AppServices.SliderAgg;
 using SM.Domain.Core.ProductAgg.AppSevices;
 using SM.Domain.Core.ProductAgg.Data;
 using SM.Domain.Core.ProductAgg.Services;
 using SM.Domain.Core.ProductCategoryAgg.AppServices;
 using SM.Domain.Core.ProductCategoryAgg.Data;
 using SM.Domain.Core.ProductCategoryAgg.Services;
+using SM.Domain.Core.SliderAgg.AppServices;
+using SM.Domain.Core.SliderAgg.Data;
+using SM.Domain.Core.SliderAgg.Services;
 using SM.Domain.Services.ProductAgg;
 using SM.Domain.Services.ProductCategoryAgg;
+using SM.Domain.Services.SlideAgg;
 using SM.Infrastructure.DataAccess.Repos.EFCore.ProductAgg;
 using SM.Infrastructure.DataAccess.Repos.EFCore.ProductCategoryAgg;
+using SM.Infrastructure.DataAccess.Repos.EFCore.SliderAgg;
 using SM.Infrastructure.DB.SqlServer.EFCore.Contexts;
 
 namespace SM.Infrastructure.Configuration
@@ -38,6 +44,17 @@ namespace SM.Infrastructure.Configuration
             services.AddScoped<IProductPictureRepository, ProductPictureRepository>();
             services.AddScoped<IProductPictureService, ProductPictureService>();
             services.AddScoped<IProductPictureAppService, ProductPictureAppService>();
+
+            #endregion
+
+            #region Slider
+
+            services.AddScoped<ISlideRepository, SlideRepository>();
+            services.AddScoped<ISlideService, SlideService>();
+            services.AddScoped<ISlideAppService, SlideAppService>();
+
+            services.AddScoped<ISlidePictureRepository, SlidePictureRepository>();
+            services.AddScoped<ISlidePictureService, SlidePictureService>();
 
             #endregion
 
