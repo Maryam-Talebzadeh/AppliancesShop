2f7122617ba5a02b32936dda5aff30a2bfaca908
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-12 03:53:15 -0700

    Wrote ISlidePictureRepository.

--- SM.Domain.Core/SliderAgg/DTOs/CreateSlidePictureDTO.cs
+++ SM.Domain.Core/SliderAgg/DTOs/CreateSlidePictureDTO.cs
@@ -1,17 +1,13 @@
-﻿using Base_Framework.Domain.Core;
-using Microsoft.AspNetCore.Http;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SM.Domain.Core.SliderAgg.DTOs
 {
-    public class SlidePictureDTO
+    public class CreateSlidePictureDTO
     {
-        public long Id { get; set; }
         public string Name { get; set; }
         public string Alt { get; set; }
         public string Title { get; set; }
---
+++ SM.Domain.Core/SliderAgg/Data/ISlidePictureRepository.cs
@@ -0,0 +1,15 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using SM.Domain.Core.ProductCategoryAgg.DTOs;
+using SM.Domain.Core.SliderAgg.DTOs;
+using SM.Domain.Core.SliderAgg.Entities;
+
+namespace SM.Domain.Core.SliderAgg.Data
+{
+    public interface ISlidePictureRepository : IRepository<SlidePicture>
+    {
+        long Create(CreateSlidePictureDTO create);
+        SlidePictureDTO GetBy(long id);
+        List<SlidePictureDTO> GetAll();
+        void Edit(SlidePictureDTO SlidePicture);
+    }
+}
