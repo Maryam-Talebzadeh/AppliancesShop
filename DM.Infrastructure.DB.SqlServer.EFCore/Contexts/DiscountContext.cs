05ec6de09481705f2628e912e0097e397637343d
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-13 12:36:28 -0700

    Created DiscountContext.

---
+++ DM.Infrastructure.DB.SqlServer.EFCore/Contexts/DiscountContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DM.Infrastructure.DB.SqlServer.EFCore.Contexts
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options) :base(options)
+        {
+            
+        }
+    }
+}
