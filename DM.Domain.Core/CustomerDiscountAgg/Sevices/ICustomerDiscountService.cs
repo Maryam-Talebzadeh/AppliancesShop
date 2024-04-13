c11a36633f56e93f7d3d747509ea7e39522bfeff
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-13 12:02:17 -0700

    Wrote ICustomerDiscountService.

---
+++ DM.Domain.Core/CustomerDiscountAgg/Sevices/ICustomerDiscountService.cs
@@ -0,0 +1,18 @@
+﻿using Base_Framework.Domain.Services;
+using DM.Domain.Core.CustomerDiscountAgg.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Core.CustomerDiscountAgg.Sevices
+{
+    public interface ICustomerDiscountService
+    {
+        OperationResult Define(DefineCustomerDiscountDTO command);
+        OperationResult Edit(EditCustoemrDiscountDTO command);
+        EditCustoemrDiscountDTO GetDetails(long id);
+        List<CustomerDiscountDTO> Search(CustomerDiscountDTO searchModel);
+    }
+}
