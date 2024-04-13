f39a095f6c271524cce3757905357819fb714889
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-13 11:58:32 -0700

    Wrote the ICustomerDiscountRepository and related DTOs.

---
+++ DM.Domain.Core/CustomerDiscountAgg/DTOs/DefineCustomerDiscountDTO.cs
@@ -0,0 +1,27 @@
+﻿using Base_Framework.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Core.CustomerDiscountAgg.DTOs
+{
+    public class DefineCustomerDiscountDTO
+    {
+        [Range(1, 100000, ErrorMessage = ValidationMessages.IsRequired)]
+        public long ProductId { get; set; }
+
+        [Range(1, 99, ErrorMessage = ValidationMessages.IsRequired)]
+        public int DiscountRate { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string StartDate { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string EndDate { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
---
+++ DM.Domain.Core/CustomerDiscountAgg/Data/ICustomerDiscountRepository.cs
@@ -0,0 +1,20 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using DM.Domain.Core.CustomerDiscountAgg.DTOs;
+using DM.Domain.Core.CustomerDiscountAgg.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Core.CustomerDiscountAgg.Data
+{
+    public interface ICustomerDiscountRepository : IRepository<CustomerDiscount>
+    {
+        CustomerDiscountDTO Get(long id);
+        void Create(DefineCustomerDiscountDTO command);
+        EditCustoemrDiscountDTO GetDetails(long id);
+        List<CustomerDiscountDTO> Search(SearchCustomerDiscountDTO searchModel);
+        void Edit(EditCustoemrDiscountDTO command);
+    }
+}
