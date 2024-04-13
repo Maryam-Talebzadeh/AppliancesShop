0afb50b0b907b75c57bbe9c894a3bd89a344b3ae
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-13 12:39:45 -0700

    Wrote configuration for CustomerDiscount.

---
+++ DM.Infrastructure.DB.SqlServer.EFCore/Configuration/CustomerDiscountConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DM.Domain.Core.CustomerDiscountAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Infrastructure.DB.SqlServer.EFCore.Configuration
+{
+    public class CustomerDiscountConfiguration : IEntityTypeConfiguration<CustomerDiscount>
+    {
+        public void Configure(EntityTypeBuilder<CustomerDiscount> builder)
+        {
+            builder.Property(x => x.Reason).HasMaxLength(500);
+        }
+    }
+}
