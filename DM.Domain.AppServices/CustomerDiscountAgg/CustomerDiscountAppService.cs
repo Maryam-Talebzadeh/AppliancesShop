8b0ab2bd1efae4718e07a6e19a5a217722567385
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-20 02:29:41 -0700

    Added ICustomerDiscountAppService and implementation of it.

---
+++ DM.Domain.AppServices/CustomerDiscountAgg/CustomerDiscountAppService.cs
@@ -0,0 +1,38 @@
+﻿using Base_Framework.Domain.Services;
+using DM.Domain.Core.CustomerDiscountAgg.AppSevices;
+using DM.Domain.Core.CustomerDiscountAgg.DTOs;
+using DM.Domain.Core.CustomerDiscountAgg.Sevices;
+
+namespace DM.Domain.AppServices.CustomerDiscountAgg
+{
+    public class CustomerDiscountAppService : ICustomerDiscountAppService
+    {
+
+        private readonly ICustomerDiscountService _customerDiscountService;
+
+        public CustomerDiscountAppService(ICustomerDiscountService customerDiscountService)
+        {
+            _customerDiscountService = customerDiscountService;
+        }
+
+        public OperationResult Define(DefineCustomerDiscountDTO command)
+        {
+            return _customerDiscountService.Define(command);
+        }
+
+        public OperationResult Edit(EditCustoemrDiscountDTO command)
+        {
+            return _customerDiscountService.Edit(command);
+        }
+
+        public EditCustoemrDiscountDTO GetDetails(long id)
+        {
+            return _customerDiscountService.GetDetails(id);
+        }
+
+        public List<CustomerDiscountDTO> Search(SearchCustomerDiscountDTO searchModel)
+        {
+            return _customerDiscountService.Search(searchModel);
+        }
+    }
+}
---
+++ DM.Domain.Core/CustomerDiscountAgg/AppSevices/ICustomerDiscountAppService.cs
@@ -0,0 +1,18 @@
+﻿using Base_Framework.Domain.Services;
+using DM.Domain.Core.CustomerDiscountAgg.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Core.CustomerDiscountAgg.AppSevices
+{
+    public interface ICustomerDiscountAppService
+    {
+        OperationResult Define(DefineCustomerDiscountDTO command);
+        OperationResult Edit(EditCustoemrDiscountDTO command);
+        EditCustoemrDiscountDTO GetDetails(long id);
+        List<CustomerDiscountDTO> Search(SearchCustomerDiscountDTO searchModel);
+    }
+}
