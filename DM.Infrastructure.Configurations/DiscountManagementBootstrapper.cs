41261741529dc2ba8cca583b45804a6029f1b81b
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-20 02:10:56 -0700

    Created DiscountManagementBootstrapper to register services in DiscountManagement module.

---
+++ DM.Infrastructure.Configurations/DiscountManagementBootstrapper.cs
@@ -0,0 +1,31 @@
+﻿
+using DM.Domain.Core.CustomerDiscountAgg.Data;
+using DM.Domain.Core.CustomerDiscountAgg.Sevices;
+using DM.Domain.Services.CustomerDiscountAgg;
+using DM.Infrastructure.DataAccess.Repos.EFCore.CustomerDiscountAgg;
+using DM.Infrastructure.DB.SqlServer.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.Infrastructure.Configuration
+{
+    public class DiscountManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<ICustomerDiscountRepository, CustomerDiscountRepository>();
+            services.AddScoped<ICustomerDiscountService, CustomerDiscountService>();
+
+            #region DBContext
+
+            services.AddDbContext<DiscountContext>(x => x.UseSqlServer(connectionString));
+
+            #endregion
+        }
+    }
+}
