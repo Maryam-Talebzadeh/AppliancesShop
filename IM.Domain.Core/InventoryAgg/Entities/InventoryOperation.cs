1b195fb511ff1f835f155a5c85104dc49556189e
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-21 09:37:39 -0700

    Wrote the Domain entities of Inventory Aggregate.

---
+++ IM.Domain.Core/InventoryAgg/Entities/InventoryOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.Domain.Core.InventoryAgg.Entities
+{
+    public class InventoryOperation
+    {
+        public long Id { get; private set; }
+        public bool Operation { get; private set; }  //if 1 then Increase, if 0 then Decrease
+        public long Count { get; private set; }
+        public long OperatorId { get; private set; }
+        public DateTime OperationDate { get; private set; }
+        public long CurrentCount { get; private set; }
+        public string Description { get; private set; }
+        public long OrderId { get; private set; }
+        public long InventoryId { get; private set; }
+        public Inventory Inventory { get; private set; }
+        protected InventoryOperation() { }
+
+        public InventoryOperation(bool operation, long count, long operatorId, long currentCount,
+            string description, long orderId, long invetoryId)
+        {
+            Operation = operation;
+            Count = count;
+            OperatorId = operatorId;
+            CurrentCount = currentCount;
+            Description = description;
+            OrderId = orderId;
+            InventoryId = invetoryId;
+            OperationDate = DateTime.Now;
+        }
+    }
+}
