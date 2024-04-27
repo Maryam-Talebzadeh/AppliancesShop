218564d9173b5360e1bfc761477e31c1025d14d7
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-27 13:21:05 -0700

    Wrote InventoryBootstrapper.

---
+++ IM.Infrastructure.Configuration/InventoryBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using IM.Domain.Core.InventoryAgg.Data;
+using IM.Infrastructure.DB.SqlServer.EFCore.contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using IM.Infrastructure.DataAccess.Repos.EFCore.InventoryAgg;
+
+namespace IM.Infrastructure.Configuration
+{
+    public class InventoryBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<IInventoryRepository, InventoryRepository>();
+
+            #region DbContext
+
+            services.AddDbContext<InventoryContext>(options =>
+            options.UseSqlServer(connectionString)
+            );
+
+            #endregion
+        }
+    }
+}
