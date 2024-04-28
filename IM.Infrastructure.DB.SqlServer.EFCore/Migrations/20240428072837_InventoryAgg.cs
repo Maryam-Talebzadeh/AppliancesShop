e422ec7eb979d7aca85296e93945c2ae8130f646
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-28 00:31:07 -0700

    Added migration for InventoryAgg.

---
+++ IM.Infrastructure.DB.SqlServer.EFCore/Migrations/20240428072837_InventoryAgg.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IM.Infrastructure.DB.SqlServer.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InventoryAgg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    UnitPrice = table.Column<double>(type: "float", nullable: false),
+                    InStock = table.Column<bool>(type: "bit", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryOperation",
+                columns: table => new
+                {
+                    InventoryId = table.Column<long>(type: "bigint", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Operation = table.Column<bool>(type: "bit", nullable: false),
+                    Count = table.Column<long>(type: "bigint", nullable: false),
+                    OperatorId = table.Column<long>(type: "bigint", nullable: false),
+                    OperationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CurrentCount = table.Column<long>(type: "bigint", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    OrderId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryOperation", x => new { x.InventoryId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_InventoryOperation_Inventory_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryOperation");
+
+            migrationBuilder.DropTable(
+                name: "Inventory");
+        }
+    }
+}
