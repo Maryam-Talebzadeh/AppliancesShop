c44af06728377c6a8390655be980e922903fa04c
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-28 00:59:26 -0700

    Introduced IInventoryService to InventoryManagementBootstrapper.

--- IM.Infrastructure.Configuration/InventoryManagementBootstrapper.cs
+++ IM.Infrastructure.Configuration/InventoryManagementBootstrapper.cs
@@ -3,6 +3,8 @@ using IM.Infrastructure.DB.SqlServer.EFCore.contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using IM.Infrastructure.DataAccess.Repos.EFCore.InventoryAgg;
+using IM.Domain.Core.InventoryAgg.Services;
+using IM.Domain.Services.InventoryAgg;
 
 namespace IM.Infrastructure.Configuration
 {
@@ -11,6 +13,7 @@ namespace IM.Infrastructure.Configuration
         public static void Configure(IServiceCollection services, string connectionString)
         {
             services.AddScoped<IInventoryRepository, InventoryRepository>();
+            services.AddScoped<IInventoryService, InventoryService>();
 
             #region DbContext
 
