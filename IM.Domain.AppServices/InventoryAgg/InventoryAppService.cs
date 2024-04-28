df4194349e1725dfd5eb645a00b320aebfb790b0
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-28 01:15:13 -0700

    Wrote InventoryAppService and implementation of it.

---
+++ IM.Domain.AppServices/InventoryAgg/InventoryAppService.cs
@@ -0,0 +1,51 @@
+﻿using IM.Domain.Core.InventoryAgg.AppServices;
+using IM.Domain.Core.InventoryAgg.DTOs;
+using IM.Domain.Core.InventoryAgg.Services;
+
+namespace IM.Domain.AppServices.InventoryAgg
+{
+    public class InventoryAppService : IInventoryAppService
+    {
+        private readonly IInventoryService _inventoryService;
+
+        public InventoryAppService(IInventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
+
+        public global::Base_Framework.Domain.Services.OperationResult Create(CreateInventoryDTO command)
+        {
+            return _inventoryService.Create(command);
+        }
+
+        public global::Base_Framework.Domain.Services.OperationResult Edit(EditInventoryDTO command)
+        {
+            return _inventoryService.Edit(command);
+        }
+
+        public EditInventoryDTO GetDetails(long id)
+        {
+            return _inventoryService.GetDetails(id);
+        }
+
+        public global::Base_Framework.Domain.Services.OperationResult Increase(IncreaseInventoryDTO command)
+        {
+            return _inventoryService.Increase(command);
+        }
+
+        public global::Base_Framework.Domain.Services.OperationResult Reduce(ReduceInventoryDTO command)
+        {
+            return _inventoryService.Reduce(command);
+        }
+
+        public global::Base_Framework.Domain.Services.OperationResult Reduce(List<ReduceInventoryDTO> command)
+        {
+            return _inventoryService.Reduce(command);
+        }
+
+        public List<InventoryDTO> Search(SearchInventoryDTO searchModel)
+        {
+            return _inventoryService.Search(searchModel);
+        }
+    }
+}
---
+++ IM.Domain.Core/InventoryAgg/AppServices/IInventoryAppService.cs
@@ -0,0 +1,16 @@
+﻿using Base_Framework.Domain.Services;
+using IM.Domain.Core.InventoryAgg.DTOs;
+
+namespace IM.Domain.Core.InventoryAgg.AppServices
+{
+    public interface IInventoryAppService
+    {
+        OperationResult Create(CreateInventoryDTO command);
+        OperationResult Edit(EditInventoryDTO command);
+        OperationResult Increase(IncreaseInventoryDTO command);
+        OperationResult Reduce(ReduceInventoryDTO command);
+        OperationResult Reduce(List<ReduceInventoryDTO> command);
+        EditInventoryDTO GetDetails(long id);
+        List<InventoryDTO> Search(SearchInventoryDTO searchModel);
+    }
+}
