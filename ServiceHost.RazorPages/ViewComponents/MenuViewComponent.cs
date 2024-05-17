6eaaa4536023a51ba1bc520dc03f592b4e520216
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-05-17 00:29:10 -0700

    Moved Menu and footer to separated ViewComponents.

---
+++ ServiceHost.RazorPages/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteQuery_Ado.Contracts;
+
+namespace ServiceHost.RazorPages.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _productCategoryQuery;
+        public MenuViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            _productCategoryQuery = productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+          
+            return View();
+        }
+    }
+}
