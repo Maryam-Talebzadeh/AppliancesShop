0a366316a7176622dd0775e74b6e60fb377d342a
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-06 07:21:34 -0700

    Created BlogContext and ArticleCategoryConfiguration.

---
+++ BM.Infrastructure.DB.SqlServer.EFCore/Contexts/BlogContext.cs
@@ -0,0 +1,28 @@
+﻿using BM.Domain.Core.ArticleCategoryAgg.Entities;
+using BM.Infrastructure.DB.SqlServer.EFCore.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM.Infrastructure.DB.SqlServer.EFCore.Contexts
+{
+    internal class BlogContext : DbContext
+    {
+        public DbSet<ArticleCategory> ArticleCategories { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(ArticleCategoryConfiguration).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
