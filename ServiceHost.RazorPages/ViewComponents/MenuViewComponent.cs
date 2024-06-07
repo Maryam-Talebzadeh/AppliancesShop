4ca3789e4b307752a00a4cde30332c62ed7bfa75
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-07 10:21:32 -0700

    Updated MenuComponent.

--- ServiceHost.RazorPages/ViewComponents/MenuViewComponent.cs
+++ ServiceHost.RazorPages/ViewComponents/MenuViewComponent.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SiteQuery_Ado.Contracts;
+using SiteQuery_Ado.Models;
 
 namespace ServiceHost.RazorPages.ViewComponents
 {
     public class MenuViewComponent : ViewComponent
     {
         private readonly IProductCategoryQuery _productCategoryQuery;
-        public MenuViewComponent(IProductCategoryQuery productCategoryQuery)
+        private readonly IArticleCategoryQuery _articleCategoryQuery;
+        public MenuViewComponent(IProductCategoryQuery productCategoryQuery, IArticleCategoryQuery articleCategoryQuery)
         {
+            _articleCategoryQuery = articleCategoryQuery;
             _productCategoryQuery = productCategoryQuery;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(CancellationToken cancellationToken)
         {
-          
-            return View();
+            var result = new MenuModel
+            {
+                ArticleCategories =await _articleCategoryQuery.GetArticleCategories(cancellationToken),
+                ProductCategories =await _productCategoryQuery.GetProductCategories(cancellationToken)
+            };
+            return View(result);
         }
     }
 }
---
+++ SiteQuery_Ado/Models/MenuModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace SiteQuery_Ado.Models
+{
+   public class MenuModel
+    {
+        public List<ArticleCategoryQueryModel> ArticleCategories { get; set; }
+        public List<ProductCategoryQueryModel> ProductCategories { get; set; }
+    }
+}
