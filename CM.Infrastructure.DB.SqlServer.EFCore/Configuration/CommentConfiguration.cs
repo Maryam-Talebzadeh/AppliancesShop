457790487688abd455f9b34a186e7e6203e2b011
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-07 22:47:20 -0700

    Added CommentContext and CommentConfiguration.

---
+++ CM.Infrastructure.DB.SqlServer.EFCore/Configuration/CommentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CM.Domain.Core.CommentAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CM.Infrastructure.DB.SqlServer.EFCore.Configuration
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(500);
+            builder.Property(x => x.Email).HasMaxLength(500);
+            builder.Property(x => x.Website).HasMaxLength(500);
+            builder.Property(x => x.Message).HasMaxLength(1000);
+        }
+    }
+}
---
+++ CM.Infrastructure.DB.SqlServer.EFCore/Contexts/CommentContext.cs
@@ -0,0 +1,21 @@
+﻿using CM.Domain.Core.CommentAgg.Entities;
+using CM.Infrastructure.DB.SqlServer.EFCore.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CM.Infrastructure.DB.SqlServer.EFCore.Contexts
+{
+    public class CommentContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+        public CommentContext(DbContextOptions<CommentContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(CommentConfiguration).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
