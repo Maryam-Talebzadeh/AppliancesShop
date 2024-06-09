d4b41802120319a1de7bfa4eaf0ffeaafe0f9058
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-09 01:28:05 -0700

    Started to write RoleAgg and created Role entity.

---
+++ AM.Domain.Core/RoleAgg/Entities/Role.cs
@@ -0,0 +1,26 @@
+﻿using AM.Domain.Core.AccountAgg.Entities;
+using Base_Framework.Domain.Core.Entities;
+
+namespace AM.Domain.Core.RoleAgg.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; private set; }
+        public List<Account> Accounts { get; private set; }
+
+        protected Role()
+        {
+        }
+
+        public Role(string name)
+        {
+            Name = name;
+            Accounts = new List<Account>();
+        }
+
+        public void Edit(string name)
+        {
+            Name = name;
+        }
+    }
+}
