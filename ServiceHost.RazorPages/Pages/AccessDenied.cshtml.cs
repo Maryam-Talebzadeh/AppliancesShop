66e273cf29149045976e1f48193a47fdd6b10d59
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-10 08:07:50 -0700

    Added AccessDenied page ---temporary---

---
+++ ServiceHost.RazorPages/Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.RazorPages.Pages
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
