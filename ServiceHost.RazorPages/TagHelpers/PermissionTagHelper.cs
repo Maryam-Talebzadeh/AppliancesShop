a5a281a9179a5bd058358377c98f942b92f148b0
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-14 05:22:34 -0700

    Wrote PermissionTagHelper.

---
+++ ServiceHost.RazorPages/TagHelpers/PermissionTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ServiceHost.RazorPages.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "Permission")]
+    public class PermissionTagHelper : TagHelper
+    {
+        public int Permission { get; set; }
+
+        private readonly IAuthHelper _authHelper;
+
+        public PermissionTagHelper(IAuthHelper authHelper)
+        {
+            _authHelper = authHelper;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!_authHelper.IsAuthenticated())
+            {
+                output.SuppressOutput();
+                return;
+            }
+
+            var permissions = _authHelper.GetPermissions();
+            if (permissions.All(x => x != Permission))
+            {
+                output.SuppressOutput();
+                return;
+            }
+
+            base.Process(context, output);
+        }
+    }
+}
