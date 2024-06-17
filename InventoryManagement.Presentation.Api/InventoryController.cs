8c9d26fd7b8fb0e3ba6f1be333600a9f6ce0bf31
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-17 07:13:52 -0700

    Added api presentation for inventory

---
+++ InventoryManagement.Presentation.Api/InventoryController.cs
@@ -0,0 +1,25 @@
+﻿using IM.Domain.Core.InventoryAgg.AppServices;
+using IM.Domain.Core.InventoryAgg.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Presentation.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryAppService _inventoryAppService;
+
+        public InventoryController( IInventoryAppService inventoryAppService)
+        {
+            _inventoryAppService = inventoryAppService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<List<InventoryOperationDTO>> GetOperationsBy(long id, CancellationToken cancellationToken)
+        {
+            return await _inventoryAppService.GetOperationLog(id, cancellationToken);
+        }
+
+    }
+}
