e51269d429ae82b9b039b832220cc55faeeaf4e2
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-17 00:30:22 -0700

    I Started to write Order aggregate, so at first I wrote Order entity.

---
+++ SM.Domain.Core/OrderAgg/Entities/Order.cs
@@ -0,0 +1,48 @@
+﻿using Base_Framework.Domain.Core.Entities;
+
+namespace SM.Domain.Core.OrderAgg.Entities
+{
+    public class Order: BaseEntity
+    {
+        public long AccountId { get; private set; }
+        public int PaymentMethod { get; private set; }
+        public double TotalAmount { get; private set; }
+        public double DiscountAmount { get; private set; }
+        public double PayAmount { get; private set; }
+        public bool IsPaid { get; private set; }
+        public bool IsCanceled { get; private set; }
+        public string IssueTrackingNo { get; private set; }
+        public long RefId { get; private set; }
+
+        public Order(long accountId, int paymentMethod, double totalAmount, double discountAmount, double payAmount)
+        {
+            AccountId = accountId;
+            TotalAmount = totalAmount;
+            DiscountAmount = discountAmount;
+            PayAmount = payAmount;
+            PaymentMethod = paymentMethod;
+            IsPaid = false;
+            IsCanceled = false;
+            RefId = 0;
+        }
+
+        public void PaymentSucceeded(long refId)
+        {
+            IsPaid = true;
+
+            if (refId != 0)
+                RefId = refId;
+        }
+
+        public void Cancel()
+        {
+            IsCanceled = true;
+        }
+
+        public void SetIssueTrackingNo(string number)
+        {
+            IssueTrackingNo = number;
+        }
+
+    }
+}
