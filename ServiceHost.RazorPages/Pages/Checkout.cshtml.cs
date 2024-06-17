0cdfa1d64c9bca67649d5217928da9205e1230d8
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-17 06:06:45 -0700

    Edited OnGet handler of CheckoutModel.

--- ServiceHost.RazorPages/Pages/Checkout.cshtml.cs
+++ ServiceHost.RazorPages/Pages/Checkout.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Nancy.Json;
 using SiteQuery_Ado.Contracts;
 using SiteQuery_Ado.Models;
+using SM.Domain.Core.OrderAgg.DTOs.Order;
+using SM.Domain.Core.OrderAgg.Services;
 
 namespace ServiceHost.RazorPages.Pages
 {
@@ -13,12 +15,14 @@ namespace ServiceHost.RazorPages.Pages
         public const string CookieName = "cart-items";
         private readonly ICartCalculatorService _cartCalculatorService;
         private readonly IAuthHelper _authHelper;
+        private readonly ICartService _cartService;
 
-        public CheckoutModel(ICartCalculatorService cartCalculatorService, IAuthHelper authHelper)
+        public CheckoutModel(ICartCalculatorService cartCalculatorService, IAuthHelper authHelper, ICartService cartService)
         {
             Cart = new CartQueryModel();
             _cartCalculatorService = cartCalculatorService;
             _authHelper = authHelper;
+            _cartService = cartService;
         }
 
         public async Task OnGet(CancellationToken cancellationToken)
@@ -30,6 +34,28 @@ namespace ServiceHost.RazorPages.Pages
                 item.CalculateTotalItemPrice();
 
             Cart =await  _cartCalculatorService.ComputeCart(cartItems, cancellationToken, _authHelper);
+            var cart = new CartDTO()
+            {
+                DiscountAmount = Cart.DiscountAmount,
+                PayAmount = Cart.PayAmount,
+                PaymentMethod = Cart.PaymentMethod,
+                TotalAmount = Cart.TotalAmount,
+                Items = Cart.Items.Select(item =>
+                new CartItemDTO()
+                {
+                    Count = item.Count,
+                    DiscountAmount = item.DiscountAmount,
+                    DiscountRate = item.DiscountRate,
+                    Id = item.Id,
+                    IsInStock = item.IsInStock,
+                    ItemPayAmount = item.ItemPayAmount,
+                    Name = item.Name,
+                    Picture = item.Picture,
+                    TotalItemPrice = item.UnitPrice,
+                    UnitPrice = item.UnitPrice
+                }).ToList()
+            };
+            await _cartService.Set(cart, cancellationToken);
         }
     }
 }
