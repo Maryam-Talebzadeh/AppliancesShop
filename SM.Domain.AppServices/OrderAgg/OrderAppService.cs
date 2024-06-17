5f0f7f95ae9877f08c5f589b4f76a9cc24397383
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-17 04:36:45 -0700

    Wrote AppService for Order.

---
+++ SM.Domain.AppServices/OrderAgg/OrderAppService.cs
@@ -0,0 +1,50 @@
+﻿using Base_Framework.Domain.Core.Contracts;
+using SM.Domain.Core.OrderAgg.AppServices;
+using SM.Domain.Core.OrderAgg.DTOs.Order;
+using SM.Domain.Core.OrderAgg.Services;
+
+namespace SM.Domain.AppServices.OrderAgg
+{
+    public class OrderAppService : IOrderAppService
+    {
+        private readonly IOrderService _orderService;
+        private readonly IAuthHelper _authHelper;
+
+        public OrderAppService(IOrderService orderService, IAuthHelper authHelper)
+        {
+            _orderService = orderService;
+            _authHelper = authHelper;
+        }
+
+        public async Task Cancel(long id, CancellationToken cancellationToken)
+        {
+           await _orderService.Cancel(id, cancellationToken);
+        }
+
+        public async Task<double> GetAmountBy(long id, CancellationToken cancellationToken)
+        {
+            return await _orderService.GetAmountBy(id, cancellationToken);
+        }
+
+        public async Task<List<OrderItemDTO>> GetItems(long orderId, CancellationToken cancellationToken)
+        {
+            return await _orderService.GetItems(orderId, cancellationToken);
+        }
+
+        public async Task<string> PaymentSucceeded(long orderId, long refId, CancellationToken cancellationToken)
+        {
+            return await _orderService.PaymentSucceeded(orderId, refId, cancellationToken);
+        }
+
+        public async Task<long> PlaceOrder(CartDTO cart, CancellationToken cancellationToken)
+        {
+            cart.AccountId =await _authHelper.CurrentAccountId(cancellationToken);
+            return await _orderService.PlaceOrder(cart, cancellationToken);
+        }
+
+        public async Task<List<OrderDTO>> Search(SearchOrderDTO searchModel, CancellationToken cancellationToken)
+        {
+            return await _orderService.Search(searchModel, cancellationToken);
+        }
+    }
+}
---
+++ SM.Domain.Core/OrderAgg/AppServices/IOrderAppService.cs
@@ -0,0 +1,14 @@
+﻿using SM.Domain.Core.OrderAgg.DTOs.Order;
+
+namespace SM.Domain.Core.OrderAgg.AppServices
+{
+    public interface IOrderAppService
+    {
+        Task<long> PlaceOrder(CartDTO cart, CancellationToken cancellationToken);
+        Task<double> GetAmountBy(long id, CancellationToken cancellationToken);
+        Task Cancel(long id, CancellationToken cancellationToken);
+        Task<string> PaymentSucceeded(long orderId, long refId, CancellationToken cancellationToken);
+        Task<List<OrderItemDTO>> GetItems(long orderId, CancellationToken cancellationToken);
+        Task<List<OrderDTO>> Search(SearchOrderDTO searchModel, CancellationToken cancellationToken);
+    }
+}
