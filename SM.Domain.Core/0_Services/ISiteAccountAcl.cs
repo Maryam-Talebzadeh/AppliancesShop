f0e18a150a647eed90025b39a4bf9621aece76e1
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-06-25 10:53:24 -0700

    Added SiteAccountAcl (following ACL pattern)

---
+++ SM.Domain.Core/0_Services/ISiteAccountAcl.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace SM.Domain.Core._0_Services
+{
+   public interface ISiteAccountAcl
+    {
+        Task<(string name, string mobile)> GetAccountBy(long id, CancellationToken cancellationToken);
+    }
+}
---
+++ SiteManagement.Infrastructure.AccountAcl/SiteAccountAcl.cs
@@ -0,0 +1,21 @@
+﻿using AM.Domain.Core.AccountAgg.AppServices;
+using SM.Domain.Core._0_Services;
+
+namespace SiteManagement.Infrastructure.AccountAcl
+{
+   public class SiteAccountAcl : ISiteAccountAcl
+    {
+        private readonly IAccountAppService _accountAppService;
+
+        public SiteAccountAcl(IAccountAppService accountAppService)
+        {
+            _accountAppService = accountAppService;
+        }
+
+        public async Task<(string name, string mobile)> GetAccountBy(long id, CancellationToken cancellationToken)
+        {
+            var account = await _accountAppService.GetAccountBy(id, cancellationToken);
+            return (account.Fullname, account.Mobile);
+        }
+    }
+}
